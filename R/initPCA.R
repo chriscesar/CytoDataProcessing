1aadaec1e87504ec7beb2a512993314e42adcea2
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-12-06 10:47:27 +0000

    Tweaked correlation plot

--- R/initPCA.R
+++ R/initPCA.R
@@ -15,6 +15,8 @@ df0 <- as_tibble(read.csv("data_in/s Measurement 2024-06-06 12h12_Listmode.csv",
 df0 %>% 
   select(., -c(Particle.ID, Sample.Length, Arrival.Time)) -> df
 
+# arrow::write_parquet(x=df0,sink="tmp/parquet.data")
+
 ## normalise data
 dfnorm <- scale(df)
 
@@ -22,8 +24,18 @@ pca01 <- princomp(na.omit(dfnorm))
 summary(pca01)
 pca01$loadings
 
+library(GGally)
+#ggpairs(na.omit(dfnorm))
+
 plot(pca01)
 # factoextra::fviz_pca(pca01)
+
+M <- cor(na.omit(dfnorm))
+corrplot::corrplot(M, method="color",type="lower")
+corrplot::corrplot(M, type="upper", order="hclust",method="color",
+                   col=brewer.pal(n=8, name="RdYlBu"),
+                   tl.col="black", tl.srt=90, tl.cex = .6)
+
 
 pca02 <- prcomp(na.omit(dfnorm), scale. = TRUE)
 
@@ -61,36 +73,36 @@ ggplot(data = scores, aes(x = PC1, y = PC2)) +
   geom_hline(yintercept = 0, linetype="dashed", col="grey")+
   geom_vline(xintercept = 0, linetype="dashed", col="grey")+
   geom_point(#aes(color = Species),
-    size = 2, shape = 19) +
-  geom_segment(
-    data = loadings, aes(
-      x = 0, y = 0,
-      xend = PC1 * scale, yend = PC2 * scale
-    ),
-    arrow = arrow(length = unit(0.3, "cm"), type = "open", angle = 25),
-    linewidth = 1, color = "darkblue"
-  )+
-  scale_x_continuous(
-    #limits = c(-3, 3),
-    name = paste0("PC1 (",round(prop_var[1]*100, digits = 2), " %)"),
-    sec.axis = sec_axis(~ . / scale, name = "Loadings on PC1")
-  ) +
-  scale_y_continuous(
-    #limits = c(-3, 3),
-    name = paste0("PC2 (",round(prop_var[2]*100, digits = 2), " %)"),
-    sec.axis = sec_axis(~ . / scale, name = "Loadings on PC2")
-  ) +
-  ggrepel::geom_label_repel(
-    data = loadings,
-    aes(
-      label = rownames(loadings),
-      x = PC1 * scale,
-      y = PC2 * scale
-    ),
-    box.padding = 0.2,
-    point.padding = 0.3,
-    size = 3, # Change the font size of the text here
-    color = "black", # Change the color of the text here
-    arrow = arrow(length = unit(0.3, "cm"), type = "closed", angle = 25),
-    force = 4
-  ) 
+    size = 2, shape = 19) #+
+  # geom_segment(
+  #   data = loadings, aes(
+  #     x = 0, y = 0,
+  #     xend = PC1 * scale, yend = PC2 * scale
+  #   ),
+  #   arrow = arrow(length = unit(0.3, "cm"), type = "open", angle = 25),
+  #   linewidth = 1, color = "darkblue"
+  # )+
+  # scale_x_continuous(
+  #   #limits = c(-3, 3),
+  #   name = paste0("PC1 (",round(prop_var[1]*100, digits = 2), " %)"),
+  #   sec.axis = sec_axis(~ . / scale, name = "Loadings on PC1")
+  # ) +
+  # scale_y_continuous(
+  #   #limits = c(-3, 3),
+  #   name = paste0("PC2 (",round(prop_var[2]*100, digits = 2), " %)"),
+  #   sec.axis = sec_axis(~ . / scale, name = "Loadings on PC2")
+  # ) +
+  # ggrepel::geom_label_repel(
+  #   data = loadings,
+  #   aes(
+  #     label = rownames(loadings),
+  #     x = PC1 * scale,
+  #     y = PC2 * scale
+  #   ),
+  #   box.padding = 0.2,
+  #   point.padding = 0.3,
+  #   size = 3, # Change the font size of the text here
+  #   color = "black", # Change the color of the text here
+  #   arrow = arrow(length = unit(0.3, "cm"), type = "closed", angle = 25),
+  #   force = 4
+  # ) 
